1dfcbf7b205b9f13903bc1f424273db4b984a0bd
AthiraKadampatta <athirakadampatta@gmail.com>
2024-11-14 12:50:49 +0530

    Fix errors while calling the class method in spec file

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -1,10 +1,10 @@
 require './lib/string_calculator.rb'
 
 describe StringCalculator do
-  describe 'add' do
+  describe '.add' do
     context 'for an empty string' do
       it 'returns 0' do
-        expect(add('')).to eq 0
+        expect(StringCalculator.add('')).to eq 0
       end
     end
   end
