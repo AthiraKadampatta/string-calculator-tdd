b8066c414e624fdb30d1d903cb10cbdd31a90832
AthiraKadampatta <athirakadampatta@gmail.com>
2024-11-14 12:54:08 +0530

    Add a failing example for a single digit scenario

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -7,5 +7,11 @@ describe StringCalculator do
         expect(StringCalculator.add('')).to eq 0
       end
     end
+
+    context 'for a string with a single digit' do
+      it 'returns the digit as it is' do
+        expect(StringCalculator.add('1')).to eq 1
+      end
+    end
   end
 end
